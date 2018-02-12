d6404ab1757f802b48f1d3a8fb0288b942e81761
iustinchenko <i.ustinchenko@digitalscreens.com.ua>
2018-02-12 11:44:04 +0200

    002

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Products from './Products';
+
+const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Sporting Goods', price: '$20.99', stocked: false, name: 'Hockey'},
+    {category: 'Sporting Goods', price: '$1.99', stocked: false, name: 'Rugby'},
+    {category: 'Sporting Goods', price: '$49.99', stocked: false, name: 'Am. Football'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus'},
+    {category: 'Electronics', price: '$299.99', stocked: true, name: 'Samsung'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'LG'},
+    {category: 'Electronics', price: '$359.99', stocked: true, name: 'Sony'}
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <section>
+          <Products products={PRODUCTS} />
+        </section>
     );
   }
 }
