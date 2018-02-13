52fb56d2aa81943b7954eb14e10894cde7d8e294
iustinchenko <i.ustinchenko@digitalscreens.com.ua>
2018-02-13 12:38:54 +0200

    009

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import logo from '../../logo.svg';
 
 class Header extends Component {
 
     render() {
         return (
             <div className='header'>
+                <a href='/'><img src={logo} alt='logo' className='logo' /></a>
+
                 {this.props.items.map((item, index) =>
                     <a href={item.link} key={index}>{item.label}</a>
                 )}
