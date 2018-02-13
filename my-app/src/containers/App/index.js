5c65e4353c6442b19e8304ba9e708115cb53f11a
iustinchenko <i.ustinchenko@digitalscreens.com.ua>
2018-02-13 12:03:37 +0200

    007

--- my-app/src/containers/App/index.js
+++ my-app/src/containers/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
 import FilterableProductTable from '../../components/FilterableProductTable/index';
 
 const PRODUCTS = [
