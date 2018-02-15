0ec957308b761a2b07f4969799f9615bf2c85d10
iustinchenko <i.ustinchenko@digitalscreens.com.ua>
2018-02-15 14:42:51 +0200

    021

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {BrowserRouter} from 'react-router-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render((
     <BrowserRouter>
-        <App />
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
     </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
