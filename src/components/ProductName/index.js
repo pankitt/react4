d4858343808459cd5b1e63e7fed5f3d8036451f6
iustinchenko <i.ustinchenko@digitalscreens.com.ua>
2018-02-15 11:06:34 +0200

    017

--- src/components/ProductName/index.js
+++ src/components/ProductName/index.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 class ProductName extends Component {
 
     render() {
-        const id = this.props.obj.match.params.number;
+        const num = this.props.obj.match.params.number;
         const products = this.props.products;
-        const item = products[id];
-        if (!item) return <div>Product not found</div>;
+        // const item = products[num];
+        const find = id => products.find(p => p.number === parseInt(id));
+        const item = find(num);
 
+        if (!item) return <div>Product not found</div>;
         return (
             <section>
                 <h3>Product name:</h3>
