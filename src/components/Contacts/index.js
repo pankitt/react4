ea383cdf6888c367b1dd09cb42191de5a0d0e0ad
i.ustinchenko <delfa07@bigmir.net>
2018-03-13 15:24:58 +0200

    032

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -15,10 +15,15 @@ class Contacts extends Component {
                     <li><a href="https://reacttraining.com/react-router/web/example/basic">React Training / React Router</a></li>
                     <li><a href="https://semantic-ui.com/introduction/getting-started.html">semantic-ui</a></li>
                     <li><a href="http://www.material-ui.com/#/components/table">material-ui</a></li>
+                    <li><a href="https://ant.design/docs/react/introduce">Ant Design</a></li>
                     <li><a href="https://www.youtube.com/channel/UCY10FZglXJ8RL3xB04VpykQ/playlists">Video 1</a></li>
                     <li><a href="https://www.youtube.com/channel/UCyUFMVZ6VnbB5a8d_EyrFBQ/playlists">Video 2</a></li>
                     <li><a href="https://maxfarseer.gitbooks.io/redux-course-ru/content/osnovi_redux.html">Основы Redux (теория)</a></li>
+                    <li><a href="https://rajdee.gitbooks.io/redux-in-russian/content/docs/introduction/">Основы Redux</a></li>
                     <li><a href="https://redux.js.org/">Redux</a></li>
+                    <li><a href="https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-css-preprocessor-sass-less-etc">
+                        Adding a CSS Preprocessor (Sass, Less etc.)
+                    </a></li>
                 </ul>
             </section>
         );
